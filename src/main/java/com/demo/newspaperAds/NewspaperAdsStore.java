f9a49b0662b19ae8654aec26edd3d287a5afbda7
nateforeman <nateforeman@hotmail.com>
2014-03-01 11:16:13 -0500

    Newspaer Ads Store

---
+++ src/main/java/com/demo/newspaperAds/NewspaperAdsStore.java
@@ -0,0 +1,72 @@
+package com.demo.newspaperAds;
+
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+
+public class NewspaperAdsStore {
+
+    private final Map<String, Set<String>> adsToNewspaperMap;
+    private final Map<String, Set<String>> newspaperToAdsMap;
+    
+    public NewspaperAdsStore() {
+        this.newspaperToAdsMap = Maps.newHashMap();
+        this.adsToNewspaperMap = Maps.newHashMap();
+    }
+    
+    public void createNewspaper(String newspaperId) {
+        if (!newspaperToAdsMap.containsKey(newspaperId)) {
+            Set<String> emptyAds = Sets.newHashSet();
+            newspaperToAdsMap.put(newspaperId, emptyAds);
+        }
+    }
+
+    public Set<String> getAllNewspapers() {
+        return ImmutableSet.copyOf((newspaperToAdsMap.keySet()));
+    }
+
+    public void createAd(String adId) {
+        if (!adsToNewspaperMap.containsKey(adId)) {
+            Set<String> emptyAds = Sets.newHashSet();
+            adsToNewspaperMap.put(adId, emptyAds);
+        }
+    }
+    
+    public Set<String> getAllAds() {
+        return ImmutableSet.copyOf((adsToNewspaperMap.keySet()));
+    }
+    
+    public void insertAd(String newspaperId, String adId) {
+        checkNewspaperExists(newspaperId);
+        checkAdExists(adId);
+        newspaperToAdsMap.get(newspaperId).add(adId);
+        adsToNewspaperMap.get(adId).add(newspaperId);
+    }
+
+    public Set<String> getAllAddsForNewspaper(String newspaperId) {
+        checkNewspaperExists(newspaperId);
+        return ImmutableSet.copyOf(newspaperToAdsMap.get(newspaperId));
+    }
+    
+    public Set<String> getAllNewspapersForAd(String adId) {
+        checkAdExists(adId);
+        return ImmutableSet.copyOf(adsToNewspaperMap.get(adId));
+    }
+    
+    private void checkNewspaperExists(String newspaperId) {
+        if (!newspaperToAdsMap.containsKey(newspaperId)) {
+            throw new IllegalArgumentException(String.format("Newspaper not found: %s", newspaperId));
+        }
+    }
+
+    private void checkAdExists(String adId) {
+        if (!adsToNewspaperMap.containsKey(adId)) {
+            throw new IllegalArgumentException(String.format("Ad not found: %s", adId));
+        }
+    }
+
+}
---
+++ src/test/java/com/demo/newspaperAds/NewspaperAdsStoreTests.java
@@ -0,0 +1,146 @@
+package com.demo.newspaperAds;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NewspaperAdsStoreTests {
+    static final String NEWSPAPER_ID_1 = "NEWS 1";
+    static final String NEWSPAPER_ID_2 = "NEWS 2";
+    
+    static final String AD_ID_1 = "AD 1";
+    static final String AD_ID_2 = "AD 2";
+
+    NewspaperAdsStore target;
+    
+    @Before
+    public void setUp() throws Exception {
+        target = new NewspaperAdsStore();
+    }
+    
+    @Test
+    public void initiallyHasNoNewspapers() throws Exception {
+        assertThat(target.getAllNewspapers().isEmpty(), is(true));
+    }
+    
+    @Test
+    public void canCreate1Newspaper() throws Exception {
+        target.createNewspaper(NEWSPAPER_ID_1);
+        assertThat(target.getAllNewspapers(), hasItem(NEWSPAPER_ID_1));
+    }
+    
+    @Test
+    public void canCreateSeveralNewspapers() throws Exception {
+        target.createNewspaper(NEWSPAPER_ID_1);
+        assertThat(target.getAllNewspapers(), hasItem(NEWSPAPER_ID_1));
+        
+        target.createNewspaper(NEWSPAPER_ID_2);
+        assertThat(target.getAllNewspapers(), hasItem(NEWSPAPER_ID_1));
+        assertThat(target.getAllNewspapers(), hasItem(NEWSPAPER_ID_2));
+    }
+    
+    @Test
+    public void initiallyHasNoAds() throws Exception {
+        assertThat(target.getAllAds().isEmpty(), is(true));
+    }
+    
+    @Test
+    public void canCreate1Ad() throws Exception {
+        target.createAd(AD_ID_1);
+        assertThat(target.getAllAds(), hasItem(AD_ID_1));
+    }
+    
+    @Test
+    public void canCreateSeveralAds() throws Exception {
+        target.createAd(AD_ID_1);
+        assertThat(target.getAllAds(), hasItem(AD_ID_1));
+        
+        target.createAd(AD_ID_2);
+        assertThat(target.getAllAds(), hasItem(AD_ID_1));
+        assertThat(target.getAllAds(), hasItem(AD_ID_2));
+    }
+    
+    @Test
+    public void cantCreateSameNewspaperMoreThanOnce() throws Exception {
+        target.createNewspaper(NEWSPAPER_ID_1);
+        target.createNewspaper(NEWSPAPER_ID_1);
+        assertThat(target.getAllNewspapers(), hasItem(NEWSPAPER_ID_1));
+    }
+    
+    @Test
+    public void cantCreateSameAddMoreThanOnce() throws Exception {
+        target.createAd(AD_ID_1);
+        target.createAd(AD_ID_1);
+        assertThat(target.getAllAds(), hasItem(AD_ID_1));
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void getAllAdsForNewspaperThatDoesntExistThrows() throws Exception {
+        target.getAllAddsForNewspaper(NEWSPAPER_ID_1);
+    }
+    
+    @Test
+    public void getAllAdsForNewspaperWithNoAdsIsEmpty() {
+        target.createNewspaper(NEWSPAPER_ID_1);
+        assertThat(target.getAllAddsForNewspaper(NEWSPAPER_ID_1).isEmpty(), is(true));
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void insertAdWhenNewspaperDoesntExistThrows() throws Exception {
+        target.createAd(AD_ID_1);
+        target.insertAd(NEWSPAPER_ID_1, AD_ID_1);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void insertAdWhenAdDoesntExistThrows() throws Exception {
+        target.createNewspaper(NEWSPAPER_ID_1);
+        target.insertAd(NEWSPAPER_ID_1, AD_ID_1);
+    }
+    
+    @Test
+    public void canInsert1AdIntoNewspaper() throws Exception {
+        target.createNewspaper(NEWSPAPER_ID_1);
+        target.createAd(AD_ID_1);
+        target.insertAd(NEWSPAPER_ID_1, AD_ID_1);
+        target.getAllAddsForNewspaper(NEWSPAPER_ID_1);
+        assertThat(target.getAllAddsForNewspaper(NEWSPAPER_ID_1), hasItem(AD_ID_1));
+    }
+    
+    @Test
+    public void canInsertSeveralAdsIntoNewspaper() throws Exception {
+        target.createNewspaper(NEWSPAPER_ID_1);
+        target.createAd(AD_ID_1);
+        target.createAd(AD_ID_2);
+        target.insertAd(NEWSPAPER_ID_1, AD_ID_1);
+        target.insertAd(NEWSPAPER_ID_1, AD_ID_2);
+        Iterable<String> matchingAds = target.getAllAddsForNewspaper(NEWSPAPER_ID_1);
+        assertThat(matchingAds, hasItem(AD_ID_1));
+        assertThat(matchingAds, hasItem(AD_ID_2));
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void getNewspapersForAdWhenAdDoesntExistThrows() throws Exception {
+        target.getAllNewspapersForAd(AD_ID_1);
+    }
+    
+    @Test
+    public void getNewspapersForAdWhenNeverInsertedReturnsEmpty() throws Exception {
+        target.createAd(AD_ID_1);
+        assertThat(target.getAllNewspapersForAd(AD_ID_1).isEmpty(), is(true));
+    }
+    
+    @Test
+    public void getNewspapersForAdWhenInsertedSeveralTimes() throws Exception {
+        target.createAd(AD_ID_1);
+        target.createNewspaper(NEWSPAPER_ID_1);
+        target.createNewspaper(NEWSPAPER_ID_2);
+        target.insertAd(NEWSPAPER_ID_1, AD_ID_1);
+        target.insertAd(NEWSPAPER_ID_2, AD_ID_1);
+        Iterable<String> matchingNewspapers = target.getAllNewspapersForAd(AD_ID_1);
+        assertThat(matchingNewspapers, hasItem(NEWSPAPER_ID_1));
+        assertThat(matchingNewspapers, hasItem(NEWSPAPER_ID_2));
+    }
+}
